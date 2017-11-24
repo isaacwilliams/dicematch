f16f3ad1dbce87033f0c1b7ee5bdb638d35b1661
Isaac Williams <isaac@isaacwilliams.net>
2017-11-24 20:18:56 +1100

    Correctly find horizontal & vertical matches

--- src/util/findBoardMatches.js
+++ src/util/findBoardMatches.js
@@ -3,28 +3,36 @@ const BOARD_HEIGHT = 5;
 const MIN_MATCH_LENGTH = 3;
 
 const getIndex = (x, y) => y * BOARD_HEIGHT + x;
+const getDieFromBoard = (board) => (x, y) => board[getIndex(x, y)];
 
-const findMatchesHorizontal = (state) => {
+const findMatchesHorizontal = (board) => {
+    const getDie = getDieFromBoard(board);
     const matchGroups = [];
+    let die;
+    let currGroup;
+    let currValue;
 
-    for (var y = 0; y < BOARD_HEIGHT; y++) {
-        let currGroup = [];
-        let currValue = state[getIndex(0, y)].value;
+    for (let y = 0; y < BOARD_HEIGHT; y++) {
+        die = getDie(0, y);
 
-        currGroup.push({ x: 0, y, value: currValue })
+        currGroup = [{ x: 0, y, ...die }];
+        currValue = die.value;
 
-        for (var x = 1; x < BOARD_WIDTH; x++) {
-            const die = state[getIndex(x, y)];
+        for (let x = 1; x < BOARD_WIDTH; x++) {
+            die = getDie(x, y);
 
-            const closeGroup = die.value !== currValue || x === (BOARD_WIDTH - 1);
+            const hasMatch = die.value === currValue;
 
-            if (die.value === currValue) {
-                currGroup.push({ x, y, value: currValue });
-            }
+            if (hasMatch) currGroup.push({ x, y, ...die });
+
+            const closeGroup = !hasMatch || x === (BOARD_WIDTH - 1);
 
             if (closeGroup && currGroup.length >= MIN_MATCH_LENGTH) {
                 matchGroups.push(currGroup);
-                currGroup = [];
+            }
+
+            if (closeGroup) {
+                currGroup = [{ x, y, ...die }];
             }
 
             currValue = die.value;
@@ -34,10 +42,46 @@ const findMatchesHorizontal = (state) => {
     return matchGroups;
 };
 
-const findMatches = (state) => {
-    const horizontalMatches = findMatchesHorizontal(state);
+const findMatchesVertical = (board) => {
+    const getDie = getDieFromBoard(board);
+    const matchGroups = [];
+    let die;
+    let currGroup;
+    let currValue;
 
-    return horizontalMatches;
+    for (let x = 0; x < BOARD_WIDTH; x++) {
+        die = getDie(x, 0);
+
+        currGroup = [{ x, y: 0, ...die }];
+        currValue = die.value;
+
+        for (let y = 1; y < BOARD_HEIGHT; y++) {
+            die = getDie(x, y);
+
+            const hasMatch = die.value === currValue;
+
+            if (hasMatch) currGroup.push({ x, y, ...die });
+
+            const closeGroup = !hasMatch || y === (BOARD_HEIGHT - 1);
+
+            if (closeGroup && currGroup.length >= MIN_MATCH_LENGTH) {
+                matchGroups.push(currGroup);
+            }
+
+            if (closeGroup) {
+                currGroup = [{ x, y, ...die }];
+            }
+
+            currValue = die.value;
+        }
+    }
+
+    return matchGroups;
 };
+
+const findMatches = (state) => ([
+    ...findMatchesVertical(state),
+    ...findMatchesHorizontal(state),
+])
 
 export default findMatches;
