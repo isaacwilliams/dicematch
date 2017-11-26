cb5fbfa097c1dacd04366c88b484a537fd97559a
Isaac Williams <isaac@isaacwilliams.net>
2017-11-26 13:03:36 +1100

    Add github pages

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
