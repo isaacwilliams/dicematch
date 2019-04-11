d9b55adb39fbf8078c50ade4bae2e4cab36e6cd6
Isaac Williams <isaac@isaacwilliams.net>
2019-04-11 20:19:37 +1000

    Slightly adjust appearance rate for random & block types

--- src/reducers/levelReducer.js
+++ src/reducers/levelReducer.js
@@ -14,8 +14,8 @@ const log = (value) => Math.log10(value);
 const getCountUp = (level) => 10 + floor(level * 0.2);
 const getCountDown = (level) => clampVal(round(log(level) * 7));
 const getCountFlip = (level) => clampVal(round(log(level) * 5) - 2);
-const getCountRandom = (level) => clampVal(round(log(level) * 4) - 3);
-const getCountBlocker = (level) => clampVal(round(log(level) * 3) - 2);
+const getCountRandom = (level) => floor(clampVal(round(log(level) * 4) - 2.5));
+const getCountBlocker = (level) => clampVal(round(log(level) * 2.5) - 2);
 
 const checkForRuns = (levelDice = []) => {
     for (var i = 0; i < levelDice.length; i++) {
