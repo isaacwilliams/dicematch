62433762622cbff78bf7da083b27fa00dd2d31a4
Isaac Williams <isaac@isaacwilliams.net>
2019-04-18 22:55:08 +1000

    Updates

---
+++ precache-manifest.677a9dffea86aecd87faa8eb379b27ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab9b5f51ac2d4afdfe24",
+    "url": "/dicematch/static/js/runtime~main.979d6832.js"
+  },
+  {
+    "revision": "78cb5b47bf0fe35c38e9",
+    "url": "/dicematch/static/js/main.61939e72.chunk.js"
+  },
+  {
+    "revision": "72cb2d7063ac1c61475e",
+    "url": "/dicematch/static/js/2.465d4060.chunk.js"
+  },
+  {
+    "revision": "78cb5b47bf0fe35c38e9",
+    "url": "/dicematch/static/css/main.23451008.chunk.css"
+  },
+  {
+    "revision": "ace67c100b3e3ce16c3ba0f0973ceb7d",
+    "url": "/dicematch/index.html"
+  }
+];
